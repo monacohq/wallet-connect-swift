7bafbc6f80770b3d9a4d6641d37e8e550842503b
Hahn.Chen <hahn.chen@crypto.com>
2021-09-20 14:32:18 +0800

    🚧 update JSONRPC data structure

--- WalletConnect/Models/WCSessionModels.swift
+++ WalletConnect/Models/WCSessionModels.swift
@@ -17,12 +17,34 @@ public struct WCSessionUpdateParam: Codable {
     public let chainId: Int?
     public let accounts: [String]?
 
+    /**
+     Current selected wallet id.
+
+     Not nil when response to our __Crypto.com extension__
+     */
+    public let selectedWalletId: String?
+    /**
+     All wallets infos
+
+     Not nil when response to our __Crypto.com extension__
+     */
+    public let wallets: [WCSessionWalletInfo]?
+
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(approved, forKey: .approved)
         try container.encode(chainId, forKey: .chainId)
         try container.encode(accounts, forKey: .accounts)
+        try container.encode(selectedWalletId, forKey: .selectedWalletId)
+        try container.encode(wallets, forKey: .wallets)
     }
+}
+
+public struct WCSessionWalletInfo: Codable {
+    public let name: String
+    public let id: String
+    public let icon: String?
+    public let address: [String : String]
 }
 
 public struct WCApproveSessionResponse: Codable {
@@ -32,4 +54,17 @@ public struct WCApproveSessionResponse: Codable {
 
     public let peerId: String?
     public let peerMeta: WCPeerMeta?
+
+    /**
+     Current selected wallet id.
+
+     Not nil when response to our __Crypto.com extension__
+     */
+    public let selectedWalletId: String?
+    /**
+     All wallets infos
+
+     Not nil when response to our __Crypto.com extension__
+     */
+    public let wallets: [WCSessionWalletInfo]?
 }
